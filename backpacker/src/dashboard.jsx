885416d8c5c6ecf842f6631efd01dd98f8f05a4e
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-09-25 02:38:28 -0300

    fix: update style

--- backpacker/src/dashboard.jsx
+++ backpacker/src/dashboard.jsx
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState,   useEffect } from 'react';
 import './style/dashboard.css';
 
 function Dashboard() {
+  const [isPopupVisible, setIsPopupVisible] = useState(true);
+
+  const closePopup = () => {
+    setIsPopupVisible(false);
+    localStorage.setItem('hasClosedPopup', 'true');
+  };
+
+  function formatarCaminho(caminhoEncontrado) {
+    // Verifica se o caminho é válido
+    if (caminhoEncontrado.length === 0) {
+      return "Caminho não encontrado";
+    }
+
+    // Cria uma string formatada com setas entre os países
+    return caminhoEncontrado.join(" -> ");
+  }
+
 
   // grafo utilizado no BFS
   const graph = {
@@ -80,46 +97,60 @@ function Dashboard() {
   
   return (
     <div className='container'>
+      <header className='header'>
+        <img src="https://cdn.pixabay.com/photo/2012/04/13/17/52/backpacker-33063_1280.png" alt="Descrição da Imagem" /> 
+        <h1>Bem-vindo, Aventureiro!</h1>
+      </header> 
+      
       <div className='filters'>
+        {isPopupVisible && (
+          <div className="popup">
+            <h3>
+              Nossa missão é ajudá-lo a traçar o caminho perfeito para a sua Eurotour dos sonhos. Sua aventura começa aqui, e o melhor caminho está a apenas um clique de distância.
+            </h3>
+            <button className='close-button' onClick={closePopup}>X</button>
+          </div>
+        )}
         <iframe
         className='iframe'
           src="https://www.google.com/maps/d/embed?mid=1mCk3QZZHGvUo-P1dgt-HBvcDJr1lhRo&ehbc=2E312F&noprof=1&iwloc=addr"
-          // src="https://www.google.com/maps/d/embed?mid=1on6Z9xUfxfyIhTU0bO4ap6XzyB_o7EE&ehbc=2E312F&noprof=1"
           width="100%"  
           height="110%"
           allowfullscreen=""
           loading="lazy"
-          // referrerpolicy="no-referrer-when-downgrade"
+          referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
-      </div>
+      </div>  
       <div className='results'>
-        <select
-          className='form-field'
-          value={origem}
-          onChange={(e) => setOrigem(e.target.value)}
-        >
-          <option value="">selecione o país de origem</option>
-          {opcoesJSON.opcoes1.map((opcao, index) => (
-            <option key={index} value={opcao}>
-              {opcao}
-            </option>
-          ))}
-        </select>
+          <div className='options'>
+          <select
+            className='form-field'
+            value={origem}
+            onChange={(e) => setOrigem(e.target.value)}
+            >
+            <option value="">selecione a cidade de origem</option>
+            {opcoesJSON.opcoes1.map((opcao, index) => (
+              <option key={index} value={opcao}>
+                {opcao}
+              </option>
+            ))}
+          </select>
 
-        <select
-          className='form-field'
-          value={destino}
-          onChange={(e) => setDestino(e.target.value)}
-        >
-          <option value="">selecione o país de destino</option>
-          {opcoesJSON.opcoes2.map((opcao, index) => (
-            <option key={index} value={opcao}>
-              {opcao}
-            </option>
-          ))}
-        </select>
+          <select
+            className='form-field'
+            value={destino}
+            onChange={(e) => setDestino(e.target.value)}
+            >
+            <option value="">selecione o país de destino</option>
+            {opcoesJSON.opcoes2.map((opcao, index) => (
+              <option key={index} value={opcao}>
+                {opcao}
+              </option>
+            ))}
+          </select>
+          </div>
         <button
-          className="button-visit en"
+          className="button-visit"
           type="button"
           onClick={encontrarMenorCaminho}
         >
@@ -130,13 +161,11 @@ function Dashboard() {
         {caminhoEncontrado.length > 0 && (
           <div>
             <h2>Caminho Encontrado:</h2>
-            <ul>
-              {caminhoEncontrado.map((pais, index) => (
-                <li key={index}>{pais}</li>
-              ))}
-            </ul>
+            <p className='line'>{formatarCaminho(caminhoEncontrado)}</p>
           </div>
         )}
+        <img className='down' src="https://cdn.pixabay.com/photo/2018/01/25/05/32/silhouette-3105461_1280.png" alt="Descrição da Imagem" /> 
+
       </div>
     </div>
   );
